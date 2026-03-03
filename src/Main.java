614afd687e36be9f1012f9c0545c7d0ed02519a5
mesak <mrai3826@zenithacademy.org>
2026-03-03 11:30:08 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the item price: $");
+        double itemPrice;
+        double shippingCost;
+
+        itemPrice = input.nextDouble();
+
+        if (itemPrice >= 100) {
+            shippingCost = 0.0;
+        }else {
+            shippingCost = itemPrice * 0.02;
+        }
+        double totalPrice = itemPrice + shippingCost;
+
+        System.out.println("the shipping cost is: $" + shippingCost);
+        System.out.printf("The total price is: $%.2f%n", totalPrice);
     }
 }
